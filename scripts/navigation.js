9dd5bcdd6fb4fef373175d7ab6b7b2e52e84674a
NeckI1T <nikitavasukov65@gmail.com>
2025-08-19 03:29:41 +0200

    #30 fix for slide-out

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,9 +4,16 @@ links.forEach(link => {
     e.preventDefault();
     const direction = link.dataset.direction;
     const href = link.getAttribute('href');
+    sessionStorage.setItem('transition', direction);
     document.body.classList.add(`slide-out-${direction}`);
     setTimeout(() => {
       window.location.href = href;
     }, 500);
   });
 });
+
+const transition = sessionStorage.getItem('transition');
+if (transition) {
+  document.body.classList.add(`slide-in-${transition}`);
+  sessionStorage.removeItem('transition');
+}
